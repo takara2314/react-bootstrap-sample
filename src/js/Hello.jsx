3ae413b8c6deed7ab5cf72e4f78bdda4d316dbcb
takara2314 <takara2314_TK@outlook.jp>
2021-07-01 19:03:32 +0900

    Add ESLint

--- src/js/Hello.jsx
+++ src/js/Hello.jsx
@@ -1,13 +1,15 @@
-import React from 'react';
+import { setState } from 'react';
 
 const Hello = () => {
+  const [greeting, setGreeting] = setState("こんにちは");
+
   return (
     <>
       <div className="p-3 mb-2 bg-primary text-white">
         Hello world!
       </div>
       <p className="mt-5 text-success">
-        こんにちは！
+        {greeting}
       </p>
     </>
   );
--- webpack.config.js
+++ webpack.config.js
@@ -1,74 +1,70 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
-module.exports = () => {
-  const config = {
-    context: path.join(__dirname, 'src'),
-    entry: './js/index.jsx',
-    output: {
-      path: __dirname,
-      filename: 'bundle.js',
-      publicPath: ''
-    },
-    module: {
-      rules: [
-        {
-          test: /\.html$/,
-          loader: 'html-loader'
-        },
-        {
-          test: /\.css/,
-          use: [
-            "style-loader",
-            {
-              loader: "css-loader",
-              options: { url: false }
+module.exports = {
+  context: path.join(__dirname, 'src'),
+  entry: './js/index.jsx',
+  output: {
+    path: __dirname,
+    filename: 'bundle.js',
+    publicPath: ''
+  },
+  module: {
+    rules: [
+      {
+        test: /\.html$/,
+        loader: 'html-loader'
+      },
+      {
+        test: /\.css/,
+        use: [
+          "style-loader",
+          {
+            loader: "css-loader",
+            options: { url: false }
+          }
+        ]
+      },
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: [
+          {
+            loader: 'esbuild-loader',
+            options: {
+              loader: 'js'
             }
-          ]
-        },
-        {
-          test: /\.js$/,
-          exclude: /node_modules/,
-          use: [
-            {
-              loader: 'esbuild-loader',
-              options: {
-                loader: 'js'
-              }
+          }
+        ]
+      },
+      {
+        test: /\.jsx$/,
+        exclude: /node_modules/,
+        use: [
+          {
+            loader: 'esbuild-loader',
+            options: {
+              loader: 'jsx'
             }
-          ]
-        },
-        {
-          test: /\.jsx$/,
-          exclude: /node_modules/,
-          use: [
-            {
-              loader: 'esbuild-loader',
-              options: {
-                loader: 'jsx'
-              }
-            }
-          ]
-        }
-      ]
-    },
-    plugins: [
-      new HtmlWebpackPlugin({
-        filename: './index.html',
-        template: './html/index.html'
-      }),
-    ],
-    mode: 'development',
-    resolve: {
-      extensions: ['.js', '.jsx']
-    },
-    devtool: 'inline-source-map',
-    devServer: {
-      contentBase: __dirname,
-      open: true,
-      port: 3000
-    }
+          }
+        ]
+      }
+    ]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      filename: './index.html',
+      template: './html/index.html'
+    }),
+  ],
+  mode: 'development',
+  resolve: {
+    extensions: ['.js', '.jsx']
+  },
+  devtool: 'inline-source-map',
+  devServer: {
+    contentBase: __dirname,
+    open: true,
+    port: 3000
+  }
   };
-
-  return config;
-};
