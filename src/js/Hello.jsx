fa2b3424cfe0bfa9c6cd4a76839827e924d6a5d1
takara2314 <takara2314_TK@outlook.jp>
2021-07-01 18:19:44 +0900

    First commit

---
+++ src/js/Hello.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const Hello = () => {
+  return (
+    <>
+      <div className="p-3 mb-2 bg-primary text-white">
+        Hello world!
+      </div>
+      <p className="mt-5 text-success">
+        こんにちは！
+      </p>
+    </>
+  );
+};
+
+export default Hello;
---
+++ src/js/index.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import { render } from 'react-dom';
+import '../css/bootstrap.min.css';
+import Hello from './Hello';
+
+render(
+  <Hello />,
+  document.getElementById('root')
+);
